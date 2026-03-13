0996802cc37475c9a9e89131cc395d7b15993676
Shravani Dakve <shravanisdakve@gmail.com>
2026-03-13 11:26:09 +0530

    feat: add src/constants/routes.js

---
+++ src/constants/routes.js
@@ -0,0 +1,9 @@
+export const ROUTES = {
+  LOGIN: '/',
+  REGISTER: '/register',
+  PROJECTS: '/projects',
+  DPR_FORM: '/projects/:projectId/dpr',
+  DPR_HISTORY: '/projects/:projectId/history',
+  ALL_REPORTS: '/reports',
+};
+
